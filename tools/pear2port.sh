04f85134a0305f390fc5c1bb945fa4624cb20e62
Bradley Giesbrecht <pixilla@macports.org>
2012-03-14 16:04:16 +0000

    Add channel support.
    git-svn-id: https://svn.macports.org/repository/macports/users/pixilla@90783 d073be05-634f-4543-b044-5fe20cf6d1d6

--- tools/pear2port.sh
+++ tools/pear2port.sh
@@ -8,28 +8,45 @@ then
     echo "Error: No package name given!"
     echo "Try something like:"
     echo "  \$ VERBOSE=yes MAINTAINERS=\"$(users) openmaintainer\" ${0} Auth"
-    echo "Or, for a version other than the most recent version"
-    echo "  \$ VERBOSE=yes MAINTAINERS=\"$(users) openmaintainer\" ${0} Auth 0.3.1"
+    echo "To specify a channel (default pear.php.net)"
+    echo "  \$ VERBOSE=yes MAINTAINERS=\"$(users) openmaintainer\" ${0} Auth pear.php.net"
+    echo "To specify a channel (required) and a version (defaults to current version)"
+    echo "  \$ VERBOSE=yes MAINTAINERS=\"$(users) openmaintainer\" ${0} Auth pear.php.net 0.3.1"
     exit 
 fi
 [ "x${VERBOSE}" == "xyes" ] && echo "Package: ${package}"
 
 if [ "x${2}" == "x" ]
 then
-version=\
-$(curl -s http://pear.php.net/package/${package}/download | \
-grep -o -P "http://download.pear.php.net/package/${package}-((?!${EXTRACT_SUFFIX}).*)${EXTRACT_SUFFIX}" | \
-sed -e "s/.*\///g" -e "s/${package}-//g" -e "s/${EXTRACT_SUFFIX}//g")
+channel=pear.php.net
 else
-version=${2}
+channel=${2}
+fi
+[ "x${VERBOSE}" == "xyes" ] && echo "Channel: ${channel}"
+
+if [ "x${3}" == "x" ]
+then
+/opt/local/lib/php/pear/bin/pear \
+    -C /Users/brad/misc/sandbox/pear/makepearports/pear.conf \
+    -c /Users/brad/misc/sandbox/pear/makepearports/pear.conf \
+    config-set default_channel ${channel}
+version=\
+$(/opt/local/lib/php/pear/bin/pear \
+    -C /Users/brad/misc/sandbox/pear/makepearports/pear.conf \
+    -c /Users/brad/misc/sandbox/pear/makepearports/pear.conf \
+    remote-info ${package} | \
+grep "Latest" | \
+awk '{print $2}')
+else
+version=${3}
 fi
 [ "x${VERBOSE}" == "xyes" ] && echo "Version: ${version}"
 
 if [ "x${version}" == "x" ]
 then
-    echo "Error: Could not determine version!"
+    echo "Error: Could not determine version for ${package}!"
     echo "Version: ${version}"
-    exit 
+    exit
 fi
 distname=${package}-${version}
 
@@ -43,7 +60,7 @@ cat << EOF > Portfile
 PortSystem          1.0
 PortGroup           php5pear 1.0
 
-php5pear.setup      ${package} ${version}
+php5pear.setup      ${package} ${version} ${channel}
 categories-append   net www
 platforms           darwin
 maintainers         ${MAINTAINERS}
@@ -55,25 +72,25 @@ EOF
 
 sleep 1
 [ "x${VERBOSE}" == "xyes" ] && echo "port -o fetch..."
-sudo port -o fetch > /dev/null 2>&1
+[ "x${VERBOSE}" == "xyes" ] && sudo port -o fetch || sudo port -o fetch > /dev/null 2>&1
 distfile=$(port -o distfiles | grep "\[${distname}${EXTRACT_SUFFIX}\]" | awk '{print $2}')
-distfile_sha1=$(openssl sha1 ${distfile} | awk '{print $2}')
 distfile_rmd160=$(openssl rmd160 ${distfile} | awk '{print $2}')
+distfile_sha256=$(openssl sha256 ${distfile} | awk '{print $2}')
 cat << EOF >> Portfile
-checksums           sha1    ${distfile_sha1} \\
-                    rmd160  ${distfile_rmd160}
+checksums           rmd160  ${distfile_rmd160} \\
+                    sha256  ${distfile_sha256}
 EOF
 
 [ "x${VERBOSE}" == "xyes" ] && echo "removing sources..."
 
 [ "x${VERBOSE}" == "xyes" ] && echo "port -o extract..."
-sudo port -o extract > /dev/null 2>&1
+[ "x${VERBOSE}" == "xyes" ] && sudo port -o extract || sudo port -o extract > /dev/null 2>&1
 deps=''
 count=0
 IFS=$'\n'
 [ "x${VERBOSE}" == "xyes" ] && echo "port -o build to check for deps..."
 sleep 1
-for dep in $(sudo port -o -v build | grep -P "${package} requires package")
+for dep in $(sudo port -o -v build | grep -P "${package} requires package" | grep -v -E '"pear/PEAR"|"pear/Archive_Tar"|"pear/Structures_Graph"|"pear/Console_Getopt"|"pear/XML_Util"')
 do
     depname=$(echo $dep | awk '{print $5}' | sed -e "s/pecl\//php5\//g" -e "s/\"//g" -e "s/\//-/g")
     count=$((count+1))
@@ -92,7 +109,7 @@ then
 fi
 unset IFS
 [ "x${VERBOSE}" == "xyes" ] && echo "port -o clean..."
-sudo port -o clean > /dev/null 2>&1
-[ "x${VERBOSE}" == "xyes" ] &&echo "final Portfile..."
+[ "x${VERBOSE}" == "xyes" ] && sudo port -o clean || sudo port -o clean > /dev/null 2>&1
+[ "x${VERBOSE}" == "xyes" ] && echo "final Portfile..."
 cat Portfile
 cd ..
